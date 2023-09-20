8eed5f4d02e8326ee188724ac30070e4f007be11
BUBA <buba86@ro.ru>
2023-09-20 20:06:43 +0500

    testNGcrash

--- src/test/java/ru/netology/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/CashbackHackServiceTest.java
@@ -36,4 +36,14 @@ public class CashbackHackServiceTest {
 
         assertEquals(actual, expected);
     }
+    @Test
+    public void testRemain3() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1000;
+
+        int actual = service.remain(amount);
+        int expected = 900;
+
+        assertEquals(actual, expected);
+    }
 }
